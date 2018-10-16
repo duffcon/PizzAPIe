0bee4ec933b355f21a75677b797d7a870c4052fa
duffcon <connerduffy@gmail.com>
2018-10-15 20:30:33 -0700

    a

--- Data/Models/Pizza.cs
+++ Data/Models/Pizza.cs
@@ -16,25 +16,17 @@ namespace PizzAPIe.Data.Models
     public class Cheese : PizzaElement { }
     public class Topping : PizzaElement { }
 
-    public class Pizza
-    {
-        public int ID { get; set; }
-        public Size Size { get; set; } 
-        public Sauce Sauce { get; set; } 
-        public Cheese Cheese { get; set; }
-        public Topping Topping { get; set; } 
-    }
-
     public class Order
     {
         public int Number { get; set; }
         public string Name { get; set; } 
         public string Phone { get; set; }
         public DateTime Time { get; set; }
-        public Pizza Pizza { get; set; }
+        public Size Size { get; set; }
+        public Sauce Sauce { get; set; }
+        public Cheese Cheese { get; set; }
+        public Topping Topping { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
     }
-
-
 }
